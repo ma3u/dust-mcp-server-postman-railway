47588e81e8037d60da3b6b17d627e7a2f8013835
Matthias Buchhorn <matthias.buchhorn@web.de>
2025-06-13 12:27:39 +0200

    Initial commit

---
+++ tools/paths.js
@@ -0,0 +1,20 @@
+export const toolPaths = [
+  'dust/dust-api-documentation/list-workspace-vaults.js',
+  'dust/dust-api-documentation/list-assistants.js',
+  'dust/dust-api-documentation/list-data-source-views.js',
+  'dust/dust-api-documentation/get-the-events-for-a-conversation.js',
+  'dust/dust-api-documentation/get-data-sources.js',
+  'dust/dust-api-documentation/search-assistants-by-name.js',
+  'dust/dust-api-documentation/get-a-conversation.js',
+  'dust/dust-api-documentation/retrieve-a-document-from-a-data-source.js',
+  'dust/dust-api-documentation/get-an-app-run.js',
+  'dust/dust-api-documentation/get-events-for-a-message.js',
+  'dust/dust-api-documentation/upsert-a-document-in-a-data-source.js',
+  'dust/dust-api-documentation/get-documents.js',
+  'dust/dust-api-documentation/create-a-new-conversation.js',
+  'dust/dust-api-documentation/create-a-message.js',
+  'dust/dust-api-documentation/create-a-content-fragment.js',
+  'dust/dust-api-documentation/create-an-app-run.js',
+  'dust/dust-api-documentation/search-the-data-source.js',
+  'dust/dust-api-documentation/search-the-data-source-view.js'
+];
